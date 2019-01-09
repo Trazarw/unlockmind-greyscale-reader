a116eaf0221a2a4d9509c0e57d7d1bc255bca108
MrVoid <enis.nefzi.sanchez@gmail.com>
2019-01-09 14:42:40 +0100

    src

---
+++ src/Unlockmind.GreyscaleReader/ConsleDisplayScaleArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Unlockmind.GreyscaleReader
+{
+    public class ConsleDisplayScaleArray
+    {
+        public ConsleDisplayScaleArray(double[,] array, int height, int width)
+        {
+            RBDisplay(array, height, width);
+            WriteArray(array, height, width);
+        }
+
+        private void RBDisplay(double[,] array, int height, int width)
+        {
+            Console.WriteLine("Red/Blue Visualization");
+            Console.ResetColor();
+            Console.WriteLine();
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    double current = array[i, j];
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    if (current > 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                        if (current > 0.51) Console.BackgroundColor = ConsoleColor.Blue;
+                        if (current.Equals(1.0)) Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    }
+                    Console.Write($"{current:0.0}|");
+                }
+                Console.ResetColor();
+                Console.WriteLine("");
+            }
+
+        }
+
+        private void WriteArray(double[,] array, int height, int with)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Raw Array");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.Write("[");
+            for (int i = 0; i < with; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    double current = array[i, j];
+                    if (i == 0 && j == 0)
+                    {
+                        Console.Write(current);
+                        continue;
+                    }
+                    Console.Write($", {current}");
+                }
+                Console.Write("]");
+            }
+            Console.ResetColor();
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ src/Unlockmind.GreyscaleReader/Greyscale2DArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Unlockmind.GreyscaleReader
+{
+    public class Greyscale2DArray
+    {
+        private const double ScaleUnit = (double) 255 * 3 / 100;
+        
+        public double[,] Value { get; }
+        public int Height { get; }
+        public int Width { get; }
+
+        public Greyscale2DArray (string path)
+        {
+            using (Image<Rgba32> image = Image.Load(path))
+            {
+                Height = image.Height;
+                Width = image.Width;
+                Value = new double[image.Width, image.Height];
+                for (int i = 0; i < image.Height; i++)
+                {
+                    for (int j = 0; j < image.Width; j++)
+                    {
+                        Rgba32 rbgaPixel = image[j, i];
+                        double value = (rbgaPixel.B + rbgaPixel.R + rbgaPixel.G) / ScaleUnit;
+                        double inverted = Math.Abs(100 - value);
+                        Value[i, j] = inverted / 100;
+                    }
+                }
+            }
+        }
+    }
+}
